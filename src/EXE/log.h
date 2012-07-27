3c9fb31a2dfba4588df27f174f233d99eec15fab
fabiankuerten@web.de <fabiankuerten@web.de@6bf4c471-38eb-e68a-c41c-03d6165592e0>
2012-07-27 15:33:41 +0000

    use c++ constructors
    git-svn-id: https://evaluatedecks.googlecode.com/svn/branches/cli@270 6bf4c471-38eb-e68a-c41c-03d6165592e0

--- src/EXE/log.h
+++ src/EXE/log.h
@@ -4,8 +4,19 @@ struct LOG_CARD
 	UCHAR DeckID;
 	UCHAR RowID;
 	UCHAR CardID;
-	LOG_CARD() { RowID = INVALID_ROWID; CardID = 100; };
-	LOG_CARD(const UCHAR deckID, const UCHAR rowID, const UCHAR cardID) { DeckID = deckID; RowID = rowID; CardID = cardID; };
+
+	// FIXME: Is it intentional that DeckID is uninitialized?
+	LOG_CARD()
+	: RowID(INVALID_ROWID),
+	  CardID(100)
+	{};
+
+	LOG_CARD(const UCHAR deckID, const UCHAR rowID, const UCHAR cardID) 
+	: DeckID(deckID),
+	  RowID(rowID),
+	  CardID(cardID)
+	{};
+
 	bool IsValid() { return (RowID != INVALID_ROWID); };
 };
 struct LOG_RECORD
@@ -14,7 +25,22 @@ struct LOG_RECORD
 	LOG_CARD Target;
 	UCHAR AbilityID;
 	UCHAR Effect;
+
+	// FIXME: Is it intentional that all fields are unitialized?
 	LOG_RECORD();
-	LOG_RECORD(LOG_CARD &src, const UCHAR abilityID, const UCHAR effect) { Src = src; AbilityID = abilityID; Effect = effect; };
-	LOG_RECORD(LOG_CARD &src, LOG_CARD &target, const UCHAR abilityID, const UCHAR effect) { Src = src; Target = target; AbilityID = abilityID; Effect = effect; };
+
+	// FIXME: Is it intentional that Target is uninitialized?
+	LOG_RECORD(LOG_CARD &src, const UCHAR abilityID, const UCHAR effect)
+	: Src(src),
+	  AbilityID(abilityID),
+	  Effect(effect)
+	{};
+
+	LOG_RECORD(LOG_CARD &src, LOG_CARD &target, const UCHAR abilityID, const UCHAR effect) 
+	: Src(src),
+	  Target(target),
+	  AbilityID(abilityID),
+	  Effect(effect)
+	{};
 };
+
