3bc7e21aa056a8830d9a0b6037571a4690046a5a
fabiankuerten@web.de <fabiankuerten@web.de@6bf4c471-38eb-e68a-c41c-03d6165592e0>
2012-07-27 15:35:48 +0000

    thanks to catepillar for spotting those
    git-svn-id: https://evaluatedecks.googlecode.com/svn/branches/cli@282 6bf4c471-38eb-e68a-c41c-03d6165592e0

--- src/EXE/compat.h
+++ src/EXE/compat.h
@@ -9,11 +9,15 @@
 
 #define strcpy_s(d,n,s) strncpy(d,s,n)
 
+// There is a ridiculous amount of names for this function.
 #define _strcmpi(a,b) strcasecmp(a,b)
 #define stricmp(a,b) strcasecmp(a,b)
+#define strcmpi(a,b) strcasecmp(a,b)
+
 #define strnicmp(a,b,n) strncasecmp(a,b,n)
 
 #define _itoa_s(i,d,l) snprintf(d,l,"%d",i)
+#define itoa(value, buf, base) sprintf(buf, "%d", value)
 
 int fopen_s(FILE** pFile, const char *filename, const char *mode) {
     FILE* file = fopen(filename, mode);
