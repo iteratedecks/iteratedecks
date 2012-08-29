8b1a2db576052a5d32767471761df1b4e7742008
fabiankuerten@web.de <fabiankuerten@web.de@6bf4c471-38eb-e68a-c41c-03d6165592e0>
2012-08-29 14:46:42 +0000

    move lowerbound,upperbound to interface
    git-svn-id: https://evaluatedecks.googlecode.com/svn/branches/cli@478 6bf4c471-38eb-e68a-c41c-03d6165592e0

--- src/CLI2/cliSimulate.cpp
+++ src/CLI2/cliSimulate.cpp
@@ -235,12 +235,6 @@ namespace EvaluateDecks {
             return x;
         }
 
-        /**
-         * p, such that B(X >= k   | p,n ) = alpha/2   (a small value)
-         * p, such that B(X <  k   | p,n ) = 1-alpha/2 (a large value)
-         * p, such that B(X <= k-1 | p,n ) = 1-alpha/2 (a large value)
-         * where B is the Binomial distribution.
-         */
         double lowerBound(unsigned int const k  //< wins
                          ,unsigned int const n  //< games
                          ,double const gamma    //< confidence level
@@ -263,10 +257,6 @@ namespace EvaluateDecks {
             }
         }
 
-        /**
-         * p, such that B(X <= k | p,n ) = alpha/2
-         * where B is the Binomial distribution.
-         */
         double upperBound(unsigned int const k  //< successfull results
                          ,unsigned int const n  //< total tries
                          ,double const gamma    //< confidence level
--- src/CLI2/cliSimulate.hpp
+++ src/CLI2/cliSimulate.hpp
@@ -7,6 +7,25 @@
 
     namespace EvaluateDecks {
         namespace CLI {
+            /**
+             * p, such that B(X >= k   | p,n ) = alpha/2   (a small value)
+             * p, such that B(X <  k   | p,n ) = 1-alpha/2 (a large value)
+             * p, such that B(X <= k-1 | p,n ) = 1-alpha/2 (a large value)
+             * where B is the Binomial distribution.
+             */
+            double lowerBound(unsigned int const k  //< wins
+                             ,unsigned int const n  //< games
+                             ,double const gamma    //< confidence level
+                             );
+
+            /**
+             * p, such that B(X <= k | p,n ) = alpha/2
+             * where B is the Binomial distribution.
+             */
+            double upperBound(unsigned int const k  //< successfull results
+                             ,unsigned int const n  //< total tries
+                             ,double const gamma    //< confidence level
+                             );
 
             RESULTS simulate(ActiveDeck const & deck1
                              ,ActiveDeck const & deck2
