d429ea6325098c83f605377b879132af04ba61b4
fabiankuerten@web.de <fabiankuerten@web.de@6bf4c471-38eb-e68a-c41c-03d6165592e0>
2012-09-07 04:47:24 +0000

    update .xml updater
    git-svn-id: https://evaluatedecks.googlecode.com/svn/branches/cli@495 6bf4c471-38eb-e68a-c41c-03d6165592e0

--- bin/updatexml.sh
+++ bin/updatexml.sh
@@ -2,7 +2,7 @@
 ASSETS_BASE_URL="http://kg.tyrantonline.com/assets/"
 
 for file in cards.xml raids.xml missions.xml quests.xml achievements.xml; do
-    LINE="wget ${ASSETS_BASE_URL}${file} -O ${file}"
+    LINE="wget --quiet ${ASSETS_BASE_URL}${file} -O ${file}"
     echo ${LINE}
     ${LINE}
 done
