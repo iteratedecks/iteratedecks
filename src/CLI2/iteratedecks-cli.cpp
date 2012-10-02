77bc0f72f36f8e9d7b5d6d54f471f18f3e983c2f
Adam Babcock <github@mrhen.com>
2012-10-02 17:45:44 -0500

    Whitespace tweaking and minor project changes.

--- src/CLI2/iteratedecks-cli.cpp
+++ src/CLI2/iteratedecks-cli.cpp
@@ -69,21 +69,21 @@ int mainWithOptions(CliOptions const & options
     }
 
     // so we do a simulation, well we need some data files first
-	DB.LoadAchievementXML("achievements.xml");
-	DB.LoadCardXML("cards.xml");
-	DB.LoadMissionXML("missions.xml");
-	DB.LoadRaidXML("raids.xml");
-	DB.LoadQuestXML("quests.xml");
+    DB.LoadAchievementXML("achievements.xml");
+    DB.LoadCardXML("cards.xml");
+    DB.LoadMissionXML("missions.xml");
+    DB.LoadRaidXML("raids.xml");
+    DB.LoadQuestXML("quests.xml");
 
     // set random generator seed. Note that if there was no argument
     // given we will set to 1, which is equivalent to not calling srand
-	srand(options.seed);
+    srand(options.seed);
 
     // TODO need to discover what this does
-	AchievementIndex = (int)(options.achievementOptions); // index, not id
+    AchievementIndex = (int)(options.achievementOptions); // index, not id
 
     // TODO will replace console output by better logging system sometime
-	bConsoleOutput = (options.verbosity > 0);
+    bConsoleOutput = (options.verbosity > 0);
 
     // Set up logging
     // TODO add support for more fine grained logging control
@@ -99,25 +99,25 @@ int mainWithOptions(CliOptions const & options
     // ... first deck
     assert(options.attackDeck.getType() == DeckArgument::HASH);
     // FIXME: should not pass c_str
-	ActiveDeck deck1(options.attackDeck.getHash().c_str(), DB.GetPointer());
-	deck1.SetOrderMatters(options.attackDeck.isOrdered());
+    ActiveDeck deck1(options.attackDeck.getHash().c_str(), DB.GetPointer());
+    deck1.SetOrderMatters(options.attackDeck.isOrdered());
 
-	RESULTS r;
+    RESULTS r;
 
-    // ... second deck
-	switch(options.defenseDeck.getType()) {
-	case DeckArgument::HASH: {
-		ActiveDeck deck2(options.defenseDeck.getHash().c_str(), DB.GetPointer());
-		deck2.SetOrderMatters(options.defenseDeck.isOrdered());
-	    r = simulate(deck1,deck2,attackLogger,defenseLogger,simulationLogger,options.numberOfIterations,options.surge);
-							 } break;
+    switch(options.defenseDeck.getType()) {
+    case DeckArgument::HASH: {
+        // ... second deck
+        ActiveDeck deck2(options.defenseDeck.getHash().c_str(), DB.GetPointer());
+        deck2.SetOrderMatters(options.defenseDeck.isOrdered());
+        r = simulate(deck1,deck2,attackLogger,defenseLogger,simulationLogger,options.numberOfIterations,options.surge);
+                             } break;
 
-	case DeckArgument::RAID_ID: {
-		r = simulateRaid(deck1, options.defenseDeck.getRaidId(), attackLogger, simulationLogger, options.numberOfIterations);
-								} break;
-	}
+    case DeckArgument::RAID_ID: {
+        r = simulateRaid(deck1, options.defenseDeck.getRaidId(), attackLogger, simulationLogger, options.numberOfIterations);
+                                } break;
+    }
 
-	printResults(r);
+    printResults(r);
 
     // Do verify, if requested.
     VerifyOptions const & verifyOptions(options.verifyOptions);
