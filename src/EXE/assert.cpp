2c852895c985e13c12c462dca7b9dd89a220b25c
Fabian <Fabian.Kuerten@rwth-aachen.de>
2012-10-27 17:53:56 +0200

    add the other assertion macros

--- src/EXE/assert.cpp
+++ src/EXE/assert.cpp
@@ -1,5 +1,6 @@
 #include "assert.hpp"
 #include <sstream>
+#include <iomanip>
 
 std::string itos(unsigned int const i) {
     std::stringstream ss;
@@ -9,7 +10,7 @@ std::string itos(unsigned int const i) {
 
 std::string dtos(double const d) {
     std::stringstream ss;
-    ss << d;
+    ss << std::fixed << std::setprecision(16) << d;
     return ss.str();
 }
 
@@ -31,16 +32,69 @@ AssertionError assertGE_(double const a
                         ,std::string const & function
                         )
 {
-    return AssertionError(sa + "== " + dtos(a) + " >= " + dtos(b) + " == " + sb,file,line,function);
+    return AssertionError(sa + " == " + dtos(a) + " >= " + dtos(b) + " == " + sb,file,line,function);
 }
+
+AssertionError assertLT_(double const a
+                        ,double const b
+                        ,std::string const & sa
+                        ,std::string const & sb
+                        ,std::string const & file
+                        ,unsigned int const line
+                        ,std::string const & function
+                        )
+{
+    return AssertionError(sa + " == " + dtos(a) + " < " + dtos(b) + " == " + sb,file,line,function);
+}
+
+AssertionError assertLT_(unsigned int const a
+                        ,unsigned int const b
+                        ,std::string const & sa
+                        ,std::string const & sb
+                        ,std::string const & file
+                        ,unsigned int const line
+                        ,std::string const & function
+                        )
+{
+    return AssertionError(sa + " == " + itos(a) + " < " + itos(b) + " == " + sb,file,line,function);
+}
+
+AssertionError assertLE_(double const a
+                        ,double const b
+                        ,std::string const & sa
+                        ,std::string const & sb
+                        ,std::string const & file
+                        ,unsigned int const line
+                        ,std::string const & function
+                        )
+{
+    return AssertionError(sa + " == " + dtos(a) + " <= " + dtos(b) + " == " + sb,file,line,function);
+}
+
+AssertionError assertLE_(unsigned int const a
+                        ,unsigned int const b
+                        ,std::string const & sa
+                        ,std::string const & sb
+                        ,std::string const & file
+                        ,unsigned int const line
+                        ,std::string const & function
+                        )
+{
+    return AssertionError(sa + " == " + itos(a) + " <= " + itos(b) + " == " + sb,file,line,function);
+}
+
+
+
 
 AssertionError assertDEE_(double const a
                          ,double const b
                          ,double const delta
+                         ,std::string const & sa
+                         ,std::string const & sb
                          ,std::string const & file
                          ,unsigned int const line
                          ,std::string const & function
                          )
 {
-    return AssertionError("doubles not close enough a=" +dtos(a) + ", b=" + dtos(b) + ", delta=" + dtos(delta),file,line,function);
+    return AssertionError("doubles not close enough "+sa+"=" +dtos(a) + ", "+sb+"=" + dtos(b) + ", delta=" + dtos(delta),file,line,function);
 }
--- src/EXE/assert.hpp
+++ src/EXE/assert.hpp
@@ -4,11 +4,14 @@
     #ifndef NDEBUG
         #include "exceptions.hpp"
         #include <string>
+        #include <cmath>
         //#include <cassert>
 
         #define assertX(b) do { if(!(b)) { throw AssertionError(#b,__FILE__,__LINE__,FUNCTION_NAME); } } while(false)
-        #define assertDEE(a,b,c) do { if(std::fabs(a-b) > c) { throw assertDEE_(a,b,c,__FILE__,__LINE__,FUNCTION_NAME); } } while(false)
+        #define assertDEE(a,b,c) do { if(std::fabs(a-b) > c) { throw assertDEE_(a,b,c,#a,#b,__FILE__,__LINE__,FUNCTION_NAME); } } while(false)
         #define assertGE(a,b) do { if(!(a>=b)) { throw assertGE_(a,b,#a,#b,__FILE__,__LINE__,FUNCTION_NAME); } } while(false)
+        #define assertLT(a,b) do { if(!(a<b )) { throw assertLT_(a,b,#a,#b,__FILE__,__LINE__,FUNCTION_NAME); } } while(false)
+        #define assertLE(a,b) do { if(!(a<=b )) { throw assertLE_(a,b,#a,#b,__FILE__,__LINE__,FUNCTION_NAME); } } while(false)
 
         #if defined(__GNUC__)
             #define FUNCTION_NAME __PRETTY_FUNCTION__
@@ -33,9 +36,43 @@
                                 ,unsigned int const line
                                 ,std::string const & function
                                 );
+        AssertionError assertLT_(double const a
+                                ,double const b
+                                ,std::string const & sa
+                                ,std::string const & sb
+                                ,std::string const & file
+                                ,unsigned int const line
+                                ,std::string const & function
+                                );
+        AssertionError assertLT_(unsigned int const a
+                                ,unsigned int const b
+                                ,std::string const & sa
+                                ,std::string const & sb
+                                ,std::string const & file
+                                ,unsigned int const line
+                                ,std::string const & function
+                                );
+        AssertionError assertLE_(double const a
+                                ,double const b
+                                ,std::string const & sa
+                                ,std::string const & sb
+                                ,std::string const & file
+                                ,unsigned int const line
+                                ,std::string const & function
+                                );
+        AssertionError assertLE_(unsigned int const a
+                                ,unsigned int const b
+                                ,std::string const & sa
+                                ,std::string const & sb
+                                ,std::string const & file
+                                ,unsigned int const line
+                                ,std::string const & function
+                                );
         AssertionError assertDEE_(double const a
                                  ,double const b
                                  ,double const delta
+                                 ,std::string const & sa
+                                 ,std::string const & sb
                                  ,std::string const & file
                                  ,unsigned int const line
                                  ,std::string const & function
