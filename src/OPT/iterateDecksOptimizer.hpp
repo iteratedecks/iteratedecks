83ac59ed9799619c2a749b44324a0d1085dbc66d
Fabian "Praetorian" Kürten <praetorian@sitl.de>
2013-05-14 01:40:08 +0200

    some design

---
+++ src/OPT/iterateDecksOptimizer.hpp
@@ -0,0 +1,12 @@
+#ifndef ITERATEDECKS_OPT_ITERATEDECKSOPTIMIZER_HPP
+    #define ITERATEDECKS_OPT_ITERATEDECKSOPTIMIZER_HPP
+
+    class Optimizer {
+
+
+        public:
+            DeckTemplate optimizeOnce(DeckTemplate & const initial);
+
+    }
+
+#end
